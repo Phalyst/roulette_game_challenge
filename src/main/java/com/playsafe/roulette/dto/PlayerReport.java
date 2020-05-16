6730fa2edf8c5948fdb4e791f3634915c9299ffd
F5312604 <phalanndwa.swethane@fnb.co.za>
2020-05-16 12:55:24 +0200

    adding Player Report for displaying the bet outcomes and winnings

---
+++ src/main/java/com/playsafe/roulette/dto/PlayerReport.java
@@ -0,0 +1,50 @@
+package com.playsafe.roulette.dto;
+
+public class PlayerReport {
+
+    private String playerName;
+    private String bet;
+    private String outcome;
+    private double winnings;
+    private int rouletteNum;
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public String getBet() {
+        return bet;
+    }
+
+    public void setBet(String bet) {
+        this.bet = bet;
+    }
+
+    public String getOutcome() {
+        return outcome;
+    }
+
+    public void setOutcome(String outcome) {
+        this.outcome = outcome;
+    }
+
+    public double getWinnings() {
+        return winnings;
+    }
+
+    public void setWinnings(double winnings) {
+        this.winnings = winnings;
+    }
+
+    public int getRouletteNum() {
+        return rouletteNum;
+    }
+
+    public void setRouletteNum(int rouletteNum) {
+        this.rouletteNum = rouletteNum;
+    }
+}
